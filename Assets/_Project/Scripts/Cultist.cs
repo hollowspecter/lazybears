4e92fd7706b5ae68f7ca0bcf192e68b13df46ded
Vivien Baguio <baguiov@posteo.net>
2021-05-15 22:27:51 +0200

    make evryone wibbly wobbly

--- Assets/_Project/Scripts/Cultist.cs
+++ Assets/_Project/Scripts/Cultist.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Cultist : MonoBehaviour
 {
+	public int idleType = 1;
+
 	private Animator animator;
 
 	private void Awake()
 	{
 		animator = GetComponent<Animator>();
+		animator.SetInteger("IdleType", idleType);
 	}
 
 	private void OnEnable()
