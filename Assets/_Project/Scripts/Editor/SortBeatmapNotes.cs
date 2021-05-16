18c51518e68ea6797e09dd2727b36974460314b0
Vivien Baguio <baguiov@posteo.net>
2021-05-16 10:35:06 +0200

    sorting notes in correct column tool, update beatmap song

---
+++ Assets/_Project/Scripts/Editor/SortBeatmapNotes.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SortBeatmapNotes
+{
+	[MenuItem("CONTEXT/Beatmap/Sort Notes")]
+	static void SortNotes(MenuCommand command)
+	{
+		Beatmap beatmap = (Beatmap)command.context;
+
+		var notes = beatmap.GetComponentsInChildren<Note>();
+		for (int i = 0; i < notes.Length; ++i)
+		{
+			if (notes[i].isCultistNote)
+			{
+				if (notes[i].direction == Note.Direction.Left)
+				{
+					notes[i].transform.localPosition = new Vector3(-6f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+				else if (notes[i].direction == Note.Direction.Down)
+				{
+					notes[i].transform.localPosition = new Vector3(-4.5f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+				else if (notes[i].direction == Note.Direction.Up)
+				{
+					notes[i].transform.localPosition = new Vector3(-3f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+				else if (notes[i].direction == Note.Direction.Right)
+				{
+					notes[i].transform.localPosition = new Vector3(-1.5f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+			}
+			else
+			{
+				if (notes[i].direction == Note.Direction.Left)
+				{
+					notes[i].transform.localPosition = new Vector3(1.5f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+				else if (notes[i].direction == Note.Direction.Down)
+				{
+					notes[i].transform.localPosition = new Vector3(3f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+				else if (notes[i].direction == Note.Direction.Up)
+				{
+					notes[i].transform.localPosition = new Vector3(4.5f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+				else if (notes[i].direction == Note.Direction.Right)
+				{
+					notes[i].transform.localPosition = new Vector3(6f, notes[i].transform.localPosition.y, notes[i].transform.localPosition.z);
+				}
+			}
+		}//endfor
+
+		EditorUtility.SetDirty(beatmap.gameObject);
+	}
+
+}
